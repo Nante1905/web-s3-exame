faa35830e7297e2032e77ffff69ff0c0848218f5
Nante <nantemino15@gmail.com>
2023-02-07 16:33:25 +0300

    front list-objet

---
+++ application/views/list-objet.php
@@ -0,0 +1,61 @@
+<?php
+defined('BASEPATH') OR exit('No direct script access allowed');
+?>
+<div class="content">
+	<div class="nav">
+		<div class="nav-item">Home</div>
+		<div class="nav-item">Mes Objets</div>
+		<div class="nav-item">Deconnexion</div>
+	</div>
+	<div class="list-objets">
+		<div class="card-objet">
+			<div class="img"><img src="" alt="Image" /></div>
+			<div class="card-info">
+				<div class="nom">Zavatra</div>
+				<div class="prix">1234</div>
+			</div>
+		</div>
+		<div class="card-objet">
+			<div class="img"><img src="" alt="Image" /></div>
+			<div class="card-info">
+				<div class="nom">Zavatra</div>
+				<div class="prix">1234</div>
+			</div>
+		</div>
+		<div class="card-objet">
+			<div class="img"><img src="" alt="Image" /></div>
+			<div class="card-info">
+				<div class="nom">Zavatra</div>
+				<div class="prix">1234</div>
+			</div>
+		</div>
+		<div class="card-objet">
+			<div class="img"><img src="" alt="Image" /></div>
+			<div class="card-info">
+				<div class="nom">Zavatra</div>
+				<div class="prix">1234</div>
+			</div>
+		</div>
+		<div class="card-objet">
+			<div class="img"><img src="" alt="Image" /></div>
+			<div class="card-info">
+				<div class="nom">Zavatra</div>
+				<div class="prix">1234</div>
+			</div>
+		</div>
+		<div class="card-objet">
+			<div class="img"><img src="" alt="Image" /></div>
+			<div class="card-info">
+				<div class="nom">Zavatra</div>
+				<div class="prix">1234</div>
+			</div>
+		</div>
+		<div class="card-objet">
+			<div class="img"><img src="" alt="Image" /></div>
+			<div class="card-info">
+				<div class="nom">Zavatra</div>
+				<div class="prix">1234</div>
+			</div>
+		</div>
+	</div>
+</div>
