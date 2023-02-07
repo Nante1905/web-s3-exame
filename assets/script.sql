faf20defc66329c3d607289ff5208d702629fc37
josselito179 <josselito.303@gmail.com>
2023-02-07 11:29:27 +0100

    insertion en categorie et utilisateur

--- assets/script.sql
+++ assets/script.sql
@@ -11,3 +11,15 @@ create table categorie (
     id int primary key not null auto_increment,
     nom varchar(100)
 );
+
+insert into utilisateur (nom,email,password) values ('admin','admin','admin'),
+                                                    ('Jean','jean@gmail.com','jean1'),
+                                                    ('Chis','chris@gmail.com','chris1'),
+                                                    ('Tony','tony@gmail.com','tony1'),
+                                                    ('Dax','dax@gmail.com','dax1');
+
+insert into categorie (nom) values ('Vetement'),
+                                    ('Eléctronique'),
+                                    ('Eléctro-ménagé'),
+                                    ('Construction'),
+                                    ('Auto-moto');
