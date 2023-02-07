6d199e3082993417885b2200fdcb14957408ca21
Grace061204 <grace.andrianisoa@gmail.com>
2023-02-07 12:10:54 +0100

    controller login && model utilsateur

---
+++ application/controllers/Login.php
@@ -0,0 +1,65 @@
+<?php
+defined('BASEPATH') or exit('No direct script access allowed');
+
+
+/**
+ *
+ * Controller Login
+ *
+ * This controller for ...
+ *
+ * @package   CodeIgniter
+ * @category  Controller CI
+ * @author    Setiawan Jodi <jodisetiawan@fisip-untirta.ac.id>
+ * @author    Raul Guerrero <r.g.c@me.com>
+ * @link      https://github.com/setdjod/myci-extension/
+ * @param     ...
+ * @return    ...
+ *
+ */
+
+class Login extends CI_Controller
+{
+    
+  public function __construct()
+  {
+    parent::__construct();
+    $this->load->model('Utilisateur_model','user',true); 
+  }
+  public function admin(){
+    $this->load->view('admin');
+  }
+  public function user(){
+    $this->load->view('client');
+  }
+
+  public function index()
+  {
+    $this->load->view('login');
+  }
+  public function auth(){
+    $email = $this->input->post('mail');
+    $mpd = $this->input->post('pass');
+
+    if($email=='admin'){
+      if($mdp=='admin'){
+        redirect('login/admin');
+      }
+    }
+    else{
+      $auth = $this->user->getClientByEmailPass($email,$mpd);
+  
+      if($auth == true){
+        redirect('home');
+      }
+      else{
+        redirect('login/user');
+      }
+    }
+  }
+
+}
+
+
+/* End of file Login.php */
+/* Location: ./application/controllers/Login.php */
---
+++ application/models/Utilisateur_model.php
@@ -0,0 +1,65 @@
+<?php
+defined('BASEPATH') OR exit('No direct script access allowed');
+
+/**
+ *
+ * Model Utilisateur_model
+ *
+ * This Model for ...
+ * 
+ * @package		CodeIgniter
+ * @category	Model
+ * @author    Setiawan Jodi <jodisetiawan@fisip-untirta.ac.id>
+ * @link      https://github.com/setdjod/myci-extension/
+ * @param     ...
+ * @return    ...
+ *
+ */
+
+class Utilisateur_model extends CI_Model {
+
+  // ------------------------------------------------------------------------
+
+  public function __construct()
+  {
+    parent::__construct();
+  }
+
+  // ------------------------------------------------------------------------
+
+
+  // ------------------------------------------------------------------------
+  public function index()
+  {
+    // 
+  }
+  public function getClientByEmailPass($email,$password){
+    $this->db->where([
+      'email' => $email,
+      'password' => $password
+    ]);
+    $query = $this->db->get('utilisateur');
+
+    $data = $query->result();
+
+    if(count($data) == 1){
+      $this->session->set_userdata('usrsession',$data[0]->email);
+      return true;
+    }
+
+    return false;
+  }
+  public function inserer($email,$mdp){
+    $data = array(
+      'email' =>$email,
+      'password'=>$mdp
+    );
+    $this->db->insert('uilisateur',$data);
+  }
+
+  // ------------------------------------------------------------------------
+
+}
+
+/* End of file Utilisateur_model.php */
+/* Location: ./application/models/Utilisateur_model.php */
