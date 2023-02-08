5e8a20fb7804c8b1f7e9d215799bee6e43fa4f9a
josselito179 <josselito.303@gmail.com>
2023-02-08 10:28:26 +0100

    footer php/css

---
+++ application/views/footer.php
@@ -0,0 +1,27 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="footer.css">
+    <title>Document</title>
+</head>
+<body>
+    
+    <div class="footer">
+        <div class="nom">
+            <p><Label> Nom et Prenom</Label></p>
+            <p>Grace</p>
+            <p>Nantenaina</p>
+            <p>Elito</p>
+        </div>
+        <div class="etu">
+            <p><Label>Numero ETU</Label></p>
+            <p>001764</p>
+            <p>001752</p>
+            <p>001773</p>
+        </div>
+    </div>
+</body>
+</html>
