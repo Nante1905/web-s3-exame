8a7c6edc0c75d2292b93cdf341f4c1c77340aefc
Grace061204 <grace.andrianisoa@gmail.com>
2023-02-08 13:22:57 +0100

    recherche front

--- application/views/list-objet.php
+++ application/views/list-objet.php
@@ -3,7 +3,19 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 ?>
 <div class="content">
 	<?php $this->load->view('templates/nav'); ?>
-	
+	<div class="search">
+		<?= form_open('') ?>
+			<input type="text" name="search" id="search">
+			<select name="categ" id="categ">
+				<?php var_dump($categorie) ?>
+				<?php for($i=0;$i<$categorie;$i++){ ?>
+					<option value=""><?= $categorie[$i]->nom; ?></option>
+			<?php
+				} ?>
+			</select>
+			<input type="submit" value="Rechercher">
+		<?= form_close(); ?>
+	</div>
 	<div class="list-objets">
 		<?php foreach($objets as $objet) { ?>
 			<div class="card-objet">
