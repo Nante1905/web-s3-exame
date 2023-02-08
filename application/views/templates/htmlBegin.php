48f8e74241c428bcd6731e3643070dff74c86f84
Nante <nantemino15@gmail.com>
2023-02-08 07:47:46 +0300

    correction listobjet css et htmlbegin

--- application/views/templates/htmlBegin.php
+++ application/views/templates/htmlBegin.php
@@ -7,10 +7,10 @@ defined('BASEPATH') OR exit('No direct script access allowed');
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link rel="stylesheet" href="<?= base_url() . '/assets/css/app.css' ?>">
+    <link rel="stylesheet" href="<?= base_url() . 'assets/css/app.css' ?>">
     <?php 
     for($i=0;$i<sizeof($style);$i++){ ?>
-        <link rel="stylesheet" href="<?= base_url() . "/assets/css/".$style[$i] .'.css' ?>">
+        <link rel="stylesheet" href="<?= base_url() . "assets/css/".$style[$i] .'.css' ?>">
     <?php
     }
     $title = isset($htmlTitle) ? $htmlTitle : 'Document';
