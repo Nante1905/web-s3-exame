58ca18647526bb36065deff04a5140013d1d5f59
Nante <nantemino15@gmail.com>
2023-02-08 14:50:24 +0300

    footer

--- application/views/footer.php
+++ application/views/footer.php
@@ -1,19 +1,6 @@
 <?php
 defined('BASEPATH') OR exit('No direct script access allowed');
-?>
-
-
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link rel="stylesheet" href="<?= base_url() ;?>footer.css">
-    <title>Document</title>
-</head>
-<body>
-    
+?>    
     <div class="footer">
         <div class="nom">
             <p><Label> Nom et Prenom</Label></p>
@@ -28,5 +15,3 @@ defined('BASEPATH') OR exit('No direct script access allowed');
             <p>001773</p>
         </div>
     </div>
-</body>
-</html>
