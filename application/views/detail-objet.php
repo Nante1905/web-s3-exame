d311b1399d1f1a5cc740ee90b0a29948927f319a
Nante <nantemino15@gmail.com>
2023-02-08 15:42:57 +0300

    delete footer duplicate

--- application/views/detail-objet.php
+++ application/views/detail-objet.php
@@ -35,17 +35,3 @@ defined('BASEPATH') OR exit('No direct script access allowed');
     </div>
 </body>
 </html>
-<div class="footer">
-        <div class="nom">
-            <p><Label> Nom et Prenom</Label></p>
-            <p>ANDRIANISOA Hariniaina Grace</p>
-            <p>ANDRIAMAZAORO Minohary Nantenaina</p>
-            <p>FANAHY MANAMPENO Joss Elito</p>
-        </div>
-        <div class="etu">
-            <p><Label>Numero ETU</Label></p>
-            <p>001764</p>
-            <p>001752</p>
-            <p>001773</p>
-        </div>
-    </div>
